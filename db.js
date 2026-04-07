5325d60349e5f77cff915f98d97826a5ed82f0b1
Mariam-Oviedo <oviedomariam2007@gmail.com>
2026-04-06 19:11:37 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,9 @@
 const mysql = require('mysql2');
 
-// Truco para que GitHub no bloquee la clave
-const p1 = 'AVNS_6Ei74OJDvA7YrddPJ4J';
-const p2 = '7Zc8GZs';
-
 const connection = mysql.createConnection({
     host: 'mysql-374da5a9-oviedomariam2007-e04.g.aivencloud.com',
     user: 'avnadmin',
-    password: p1 + p2, // Aquí las unimos de nuevo
+    password: 'AVNS_6Ei74OJDvA7YrddPJ4J',
     database: 'defaultdb',
     port: 16403,
     ssl: {
@@ -20,7 +16,7 @@ connection.connect(err => {
         console.error('Error de conexion:', err.message);
         return;
     }
-    console.log('Conectado a Aiven exitosamente');
+    console.log('¡Conectado exitosamente a la base de datos de Aiven!');
 });
 
 module.exports = connection;
